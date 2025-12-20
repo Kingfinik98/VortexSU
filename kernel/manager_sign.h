f0d2adba1e37e91852f90b7e95d48fb91928cc8b
YC酱luyancib <luyancib@qq.com>
2025-12-20 14:06:47 +0800

    kernel:use our keys

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -6,6 +6,11 @@
 #define EXPECTED_HASH_SHIRKNEKO                                                \
     "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef"
 
+// ReSukiSU/ReSukiSU
+#define EXPECTED_SIZE_RESUKISU 0x377
+#define EXPECTED_HASH_RESUKISU                                                 \
+    "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"
+
 // Dynamic Sign
 #define EXPECTED_SIZE_OTHER 0x300
 #define EXPECTED_HASH_OTHER                                                    \
