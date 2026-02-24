8e2918a3b25ad4e8df84bbd832e6f1900f6e6645
kingfinik98 <kingfinix98@gmail.com>
2026-02-24 22:29:54 +0700

    Fix: Tambah import Image yang hilang

--- manager/app/src/main/java/com/vortexsu/vortexsu/ui/screen/Home.kt
+++ manager/app/src/main/java/com/vortexsu/vortexsu/ui/screen/Home.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.height
+import androidx.compose.foundation.Image
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
